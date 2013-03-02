da8dee693be06efac7d787c20e15e93b42c43d9e
Daniel R. Slater <dan_slater@yahoo.com>
2013-03-03 01:58:32 +0500

    file moves

---
+++ ThreadPool/IncI/stdafx.h
@@ -0,0 +1,50 @@
+#pragma once
+#ifndef __STDAFX_H__
+#define __STDAFX_H__
+
+#define _WIN32_WINNT _WIN32_WINNT_WIN7
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include <crtdbg.h>
+#include <new>
+
+
+#pragma warning( disable : 4995 )
+#include <cstdio>
+#include <cstring>
+#include <cwchar>
+#include <tchar.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Strsafe.h>
+#include <intrin.h>
+#include <memory>
+#include <functional>
+
+#include <string>
+
+#if defined(_UNICODE) || defined(UNICODE)
+#define _tstring wstring
+#else
+#define _tstring string
+#endif
+
+#pragma warning( default : 4995 )
+
+#ifdef _DEBUG
+
+void TRACE(LPCTSTR lpszFormat, ...);
+
+#define THIS_FILE __FILE__
+#define DEBUG_NEW new(_NORMAL_BLOCK, THIS_FILE, __LINE__)
+
+#else
+
+#define TRACE __noop
+
+#endif
+
+#define THREADPOOL_API __declspec(dllexport)
+
+#endif // __STDAFX_H__
